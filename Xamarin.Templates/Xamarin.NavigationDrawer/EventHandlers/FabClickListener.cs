03bb1979980f3717b1915c7393e0820f33ffe149
davide lettieri <davide.lettieri@hotmail.it>
2017-10-07 16:29:47 +0200

    ported all the project from the google template

---
+++ Xamarin.Templates/Xamarin.NavigationDrawer/EventHandlers/FabClickListener.cs
@@ -0,0 +1,14 @@
+﻿using Android.Support.Design.Widget;
+using Android.Views;
+
+namespace Xamarin.NavigationDrawer.EventHandlers
+{
+    public class FabClickListener : Java.Lang.Object, View.IOnClickListener
+    {
+        public void OnClick(View v)
+        {
+            Snackbar.Make(v, "Replace with your own action", Snackbar.LengthLong)
+                      .SetAction("Action", (View.IOnClickListener)null).Show();
+        }
+    }
+}
--- Xamarin.Templates/Xamarin.NavigationDrawer/MainActivity.cs
+++ Xamarin.Templates/Xamarin.NavigationDrawer/MainActivity.cs
@@ -1,18 +1,108 @@
-﻿using Android.App;
-using Android.Widget;
+﻿using System;
+using Android.App;
 using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Xamarin.NavigationDrawer.EventHandlers;
+using Android.Support.V4.Widget;
+using Android.Support.V4.View;
 
 namespace Xamarin.NavigationDrawer
 {
-    [Activity(Label = "Xamarin.NavigationDrawer", MainLauncher = true)]
-    public class MainActivity : Activity
+    [Activity(Label = "Xamarin.NavigationDrawer", MainLauncher = true, Theme = "@style/AppTheme.NoActionBar")]
+    public class MainActivity : AppCompatActivity, NavigationView.IOnNavigationItemSelectedListener
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
+            SetContentView(Resource.Layout.activity_main);
+
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            var fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+
+            fab.SetOnClickListener(new FabClickListener());
+
+            var drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            var toggle = new ActionBarDrawerToggle(this, drawer, toolbar, Resource.String.navigation_drawer_open, Resource.String.navigation_drawer_close);
+
+            // this is deprecated, don't know how to fix this yet
+            drawer.SetDrawerListener(toggle);
+
+            toggle.SyncState();
+
+            NavigationView navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            navigationView.SetNavigationItemSelectedListener(this);
+        }
+
+        public bool OnNavigationItemSelected(IMenuItem menuItem)
+        {
+            int id = menuItem.ItemId;
+
+            if (id == Resource.Id.nav_camera)
+            {
+                // Handle the camera action
+            }
+            else if (id == Resource.Id.nav_gallery)
+            {
+
+            }
+            else if (id == Resource.Id.nav_slideshow)
+            {
+
+            }
+            else if (id == Resource.Id.nav_manage)
+            {
+
+            }
+            else if (id == Resource.Id.nav_share)
+            {
+
+            }
+            else if (id == Resource.Id.nav_send)
+            {
+
+            }
+
+            DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            drawer.CloseDrawer(GravityCompat.Start);
+            return true;
+        }
+
+        public override void OnBackPressed()
+        {
+            var drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+
+            if (drawer.IsDrawerOpen(GravityCompat.Start))
+            {
+                drawer.CloseDrawer(GravityCompat.Start);
+            }
+            else
+            {
+                base.OnBackPressed();
+            }
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+
+            if(item.ItemId == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
         }
     }
 }
