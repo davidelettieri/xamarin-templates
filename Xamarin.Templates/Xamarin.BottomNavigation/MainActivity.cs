f186e4417c5f49fd17af51ada903fc0f23366722
davidelettieri <davidelettieri@users.noreply.github.com>
2017-10-13 22:45:17 +0200

    bottom navigation ported from android studio template

--- Xamarin.Templates/Xamarin.BottomNavigation/MainActivity.cs
+++ Xamarin.Templates/Xamarin.BottomNavigation/MainActivity.cs
@@ -1,18 +1,45 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.Design.Widget;
+using Android.Views;
 
 namespace Xamarin.BottomNavigation
 {
-    [Activity(Label = "Xamarin.BottomNavigation", MainLauncher = true)]
-    public class MainActivity : Activity
+    [Activity(Label = "Xamarin.BottomNavigation", MainLauncher = true, Theme = "@style/AppTheme")]
+    public class MainActivity : AppCompatActivity, BottomNavigationView.IOnNavigationItemSelectedListener
     {
+        private TextView _textMessage;
+
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.navigation_home:
+                    _textMessage.Text = GetString(Resource.String.title_home);
+                    return true;
+                case Resource.Id.navigation_dashboard:
+                    _textMessage.Text = GetString(Resource.String.title_dashboard);
+                    return true;
+                case Resource.Id.navigation_notifications:
+                    _textMessage.Text = GetString(Resource.String.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            _textMessage = FindViewById<TextView>(Resource.Id.message);
+
+            var navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
+
+            navigation.SetOnNavigationItemSelectedListener(this);
         }
     }
 }
